6097420f37ea9880267b5c2d7626e4b6544ca5e0
Laptop <laptop@Laptops-MacBook.home>
2018-03-19 12:39:23 -0400

    fix to reactGA

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react'
 import {
-  BrowserRouter as Router,
+  Router,
 } from 'react-router-dom'
 import ReactGA from 'react-ga'
+import createHistory from 'history/createBrowserHistory'
 
 import { RoutesWithRouter as Routes } from './components/Routes'
 
 ReactGA.initialize('UA-37325539-1')
+ReactGA.set({ page: window.location.pathname })
+ReactGA.pageview( window.location.pathname)
+
+const history = createHistory()
+history.listen((location, action) => {
+  ReactGA.set({ page: location.pathname })
+  ReactGA.pageview(location.pathname)
+  console.log(location.pathname)
+})
 
 export default class App extends Component {
 
   render() {
     return (
 
-      <Router onUpdate={ logPageView }>
+      <Router history={ history }>
         <Routes />
       </Router>
 
@@ -21,10 +31,4 @@ export default class App extends Component {
 
   }
 
-}
-
-function logPageView() {
-    var path = /\/.*(?=\?)/.exec(window.location.hash)[0]
-    ReactGA.set({ page: path });
-    ReactGA.pageview(path);
 }
