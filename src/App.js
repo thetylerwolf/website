e79f766d7e80590858698e317d83d24f14410c7d
Laptop <laptop@Laptops-MacBook.home>
2018-03-20 13:16:03 -0400

    removed react ga

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React, { Component } from 'react'
 import {
   Router,
 } from 'react-router-dom'
-import ReactGA from 'react-ga'
+
 import createHistory from 'history/createBrowserHistory'
 
 import { RoutesWithRouter as Routes } from './components/Routes'
 
-ReactGA.initialize('UA-37325539-1')
-ReactGA.set({ page: window.location.pathname })
-ReactGA.pageview( window.location.pathname)
-
 const history = createHistory()
 history.listen((location, action) => {
-  ReactGA.set({ page: location.pathname })
-  ReactGA.pageview(location.pathname)
+  window.gtag('config', 'UA-37325539-1', {
+    'page_title' : location.pathname,
+    'page_path': location.pathname
+  });
   console.log(location.pathname)
 })
 
