a33ef861764296c9f62b0b5414bb305a172c7421
thetylerwolf <laptop@laptops-mbp.lan>
2023-08-29 20:49:12 +0200

    fix to corr disp viz link

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,57 +1,58 @@
-import React, { Component } from 'react'
-import {
-  Route,
-  Switch,
-} from 'react-router-dom'
-import { withRouter } from 'react-router'
-import {
-  CSSTransition,
-  TransitionGroup,
-} from 'react-transition-group'
+import React, { Component } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { withRouter } from "react-router";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-import Home from './Home'
-import About from './About'
-import Contact from './Contact'
-import ViewPortfolioPiece from './ViewPortfolioPiece'
-import Legacy from './Legacy'
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import ViewPortfolioPiece from "./ViewPortfolioPiece";
+import Legacy from "./Legacy";
 
 export class Routes extends Component {
-
   scrollToTop() {
-    if (this.props.history.action !== 'POP') {
-      window.scrollTo(0, 0)
+    if (this.props.history.action !== "POP") {
+      window.scrollTo(0, 0);
     }
   }
 
   render() {
     return (
-          <main>
-            <TransitionGroup>
-              <CSSTransition
-                key={ this.props.location.key }
-                mountOnEnter={ true }
-                unmountOnExit={ true }
-                timeout={ 300 }
-                classNames="page-level"
-                onExited={ () => this.scrollToTop() }
-              >
-                <Switch location={ this.props.location }>
-                  <Route path="/" exact component={ Home }/>
-                  <Route path="/home" exact component={ Home }/>
-                  <Route path="/about" component={ About }/>
-                  <Route path="/contact" component={ Contact }/>
-                  <Route path="/portfolio/:id" component={ ViewPortfolioPiece }/>
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
-          </main>
-
-    )
-
+      <main>
+        <TransitionGroup>
+          <CSSTransition
+            key={this.props.location.key}
+            mountOnEnter={true}
+            unmountOnExit={true}
+            timeout={300}
+            classNames="page-level"
+            onExited={() => this.scrollToTop()}
+          >
+            <Switch location={this.props.location}>
+              <Route
+                path="/"
+                exact
+                render={({ location }) => {
+                  const { hash } = location;
+                  if (hash === "#/portfolio/corrdisp") {
+                    window.location = "/corrdisp/index.html";
+                  } else {
+                    return <Home />;
+                  }
+                }}
+              />
+              <Route path="/home" exact component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/portfolio/:id" component={ViewPortfolioPiece} />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      </main>
+    );
   }
-
 }
 
-const RoutesWithRouter = withRouter(Routes)
+const RoutesWithRouter = withRouter(Routes);
 
-export { RoutesWithRouter }
+export { RoutesWithRouter };
