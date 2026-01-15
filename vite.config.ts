7d6d08b42782fa639cd89ebc423d53614130757e
Tyler Wolf <tyler@wovensignal.xyz>
2026-01-15 11:22:39 +0100

    added github-pages action

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.GITHUB_REPOSITORY 
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+    : '/',
   server: {
     host: "::",
     port: 8080,
