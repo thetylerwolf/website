13b2812876d1013520c5b8a1328205154cd792b9
Tyler Wolf <tyler@wovensignal.xyz>
2026-01-15 11:40:55 +0100

    update to project base path for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.GITHUB_REPOSITORY 
-    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
-    : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
